93383ac043f408c548264b9febd3b4c00001f9e9
vikumR <vikumravishanka@gmail.com>
2020-04-15 10:54:38 +0530

    #app with completed CRUD
    Signed-off-by: vikumR <vikumravishanka@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ http://rogerdudler.github.io/git-guide/
 
 #Tutorial
 https://www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ
+
+#SQLite tutorial
+https://grokonez.com/flutter/flutter-sqlite-example-crud-sqflite-example
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:learn_git_app/pages/content.dart';
 import 'package:learn_git_app/pages/toc.dart';
+import 'package:learn_git_app/pages/note.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/home',
   routes: {
     '/home': (context) => Toc(),
     '/content': (context) => Content(),
-  }
+    '/note': (context) => Note(),
+  },
+  debugShowCheckedModeBanner: false
 ));
 
 
