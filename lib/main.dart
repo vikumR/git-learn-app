c9b78989707665962144350b2a4b3a895d3969aa
vikumR <47911836+vikumR@users.noreply.github.com>
2020-05-03 12:46:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
+/*
+IT17009164-Mahanthe V.R.B.
+IT17091626-P.G.C.B. Samarakoon
+References: 1. https://flutter.dev/docs
+            2. www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ
+            3. https://grokonez.com/flutter/flutter-sqlite-example-crud-sqflite-example
+*/
+
 import 'package:flutter/material.dart';
 import 'package:learn_git_app/pages/content.dart';
 import 'package:learn_git_app/pages/toc.dart';
